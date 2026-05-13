98620743ab94f8a0441ba8b998c51b1011f91b3f
Jair Granizo <steven.granizo@udla.edu.ec>
2026-05-13 14:27:41 -0500

    Tercer commit

--- punteros.c
+++ punteros.c
@@ -2,12 +2,17 @@
 
 int main () {
 
-    
+
     int vec[5]={2,4,6,8,10};
+    int *ptr;
+    ptr=&vec[0];
+
+    //printf ("%p", ptr);
 
    for (int i = 0; i<5; i++)
    {
-    printf ("%d", vec[i]);
+    printf ("vector[%d] %d ",i, vec[i]);
+    printf ("%d\n", *(ptr++));
    }
    
 
