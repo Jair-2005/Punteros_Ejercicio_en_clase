a6022aa31f1af8c63499845fc2e93aeef834f355
Jair Granizo <steven.granizo@udla.edu.ec>
2026-05-13 15:30:06 -0500

    tercer programa-segundo commit

--- ejercicio3.c
+++ ejercicio3.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
 int main () {
-    char tab[]= {'a', 'e','i','o','u'};
-    int i = sizeof tab;
-    printf("%d",i);
-
+    printf("this computing enveiroment uses \n");
+    printf("%zu byte for chars\n ", sizeof(char));
+    printf("%zu bytes for shorts\n", sizeof(short int));
+    printf("%zu byte for ints\n ", sizeof(int));
+    printf("%zu bytes for longs\n", sizeof(long int));
+    printf("%zu byte for long long\n ", sizeof(long long int));
+    printf("%zu bytes for floats\n", sizeof(float));
+    printf("%zu bytes for doubles\n", sizeof(double));
+    printf("%zu byte for pinters\n ", sizeof(int*));
+    
+    return 0;
 }
