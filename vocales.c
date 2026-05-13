5d3a9cd71941379cdaf7b0ad59af4881cdfbec24
Jair Granizo <steven.granizo@udla.edu.ec>
2026-05-13 14:44:12 -0500

    Segundo programa-Segundo commit

---
+++ vocales.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main () {
+
+    char vocales[5]= {'a', 'e','i','o','u'};
+    char *ptr;
+    ptr=&vocales[0];
+        for (int i = 0; i < 5; i++)
+        {
+            printf ("vocales[%d] %c\n",i, vocales[i]);
+            printf ("%c\n", *(ptr++));
+        }
+        
+    return 0;
+}
